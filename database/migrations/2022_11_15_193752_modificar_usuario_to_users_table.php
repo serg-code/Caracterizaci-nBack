5bcee948580f7426b5c19b79e2100e1ad58bda5c
desteban <esteban28c@hotmail.com>
2022-11-15 19:40:44 -0500

    agregar campo activo en la tabla de usuarios

--- database/migrations/2022_11_15_193752_modificar_usuario_to_users_table.php
+++ database/migrations/2022_11_15_193752_modificar_usuario_to_users_table.php
@@ -13,15 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table)
+        Schema::table('users', function (Blueprint $table)
         {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->boolean('ñ')->default(true)->after('password');
         });
     }
 
@@ -32,6 +26,9 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::table('users', function (Blueprint $table)
+        {
+            $table->dropColumn('ñ');
+        });
     }
 };
