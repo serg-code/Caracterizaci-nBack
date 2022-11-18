0cb1e52278104392d936f15971dbb34808a0efae
desteban <esteban28c@hotmail.com>
2022-11-18 17:23:06 -0500

    arreglar columnas de las tablas

--- database/migrations/2022_11_16_171629_create_factores_protectores_table.php
+++ database/migrations/2022_11_16_171629_create_factores_protectores_table.php
@@ -16,32 +16,21 @@ return new class extends Migration
         Schema::create('factores_protectores', function (Blueprint $table)
         {
             $table->char('hogar_id', 36);
-            $table->string('Tipo_familia');
-            $table->string('Duermen_niños_niñas_adultos');
-            $table->string('Problemas_alcohol');
-            $table->string('Consume_tranquilizantes');
-            $table->string('Relaciones_cordiales_respetuosasa');
-            $table->string('Lavar_manos_antes_de_comer');
-            $table->string('Lavar_manos_antes_de_preparar_alimentos');
-            $table->string('Fumigar_vivienda');
-            $table->string('Secretaría_ha_fumigado');
-            $table->string('Acido_borico_cucarachas');
+            $table->string('tipo_familia');
+            $table->string('duermen_niños_niñas_adultos');
+            $table->string('problemas_alcohol');
+            $table->string('consume_tranquilizantes');
+            $table->string('relaciones_cordiales_respetuosasa');
+            $table->string('lavar_manos_antes_comer');
+            $table->string('lavar_manos_antes_preparar_alimentos');
+            $table->string('fumigar_vivienda');
+            $table->string('secretaría_fumigado');
+            $table->string('acido_borico_cucarachas');
 
             $table->timestamps();
 
 
             $table->foreign('hogar_id')->references('id')->on('hogar');
-            // $table->foreign([
-            //     'Tipo_familia',
-            //     'Duermen_niños_niñas_adultos',
-            //     'Problemas_alcohol',
-            //     'Consume_tranquilizantes',
-            //     'Relaciones_cordiales_respetuosasa',
-            //     'Lavar_manos_antes_de_comer',
-            //     'Lavar_manos_antes_de_preparar_alimentos',
-            //     'Fumigar_vivienda', 'Secretaría_ha_fumigado',
-            //     'Acido_borico_cucarachas',
-            // ])->references('ref_campo')->on('preguntas');
         });
     }
 
