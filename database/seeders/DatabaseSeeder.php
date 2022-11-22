7fa0da69292531f006a98afaa9c2b42a86adc894
_Sergiobg <sandresbg@hotmail.com>
2022-11-22 09:36:11 -0500

    correcion opciones de respuesta

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $listadoSeeders = [
             new RolesSeeder(),
             new PreguntaSeeder(),
+            new OpcionesSeeder(),
         ];
 
         $this->correrSeeders($listadoSeeders);
