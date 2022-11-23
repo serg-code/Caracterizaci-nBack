e6d8cb18287ee9928c625b206478d0813a8debbd
desteban <esteban28c@hotmail.com>
2022-11-22 22:01:01 -0500

    modelo factoresProtectores

---
+++ app/Models/secciones/FactoresProtectores.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models\secciones;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FactoresProtectores extends Model
+{
+    use HasFactory;
+
+    protected $table = 'factores_protectores';
+
+    protected $fillable = [
+        'hogar_id',
+        'tipo_familia',
+        'duermen_ninos_ninas_adultos',
+        'problemas_alcohol',
+        'consume_tranquilizantes',
+        'relaciones_cordiales_respetuosasa',
+        'lavar_manos_antes_comer',
+        'lavar_manos_antes_preparar_alimentos',
+        'fumigar_vivienda',
+        'secretaria_fumigado',
+        'acido_borico_cucarachas',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+}
