8670694cf08f00adbad3e89207fe11bf6755dd5d
_Sergiobg <sandresbg@hotmail.com>
2022-11-23 15:28:44 -0500

    agregar seeder a la lista

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,8 @@ class DatabaseSeeder extends Seeder
             new OpcionesSeeder(),
             new IdentificacionSeeder(),
             new ParentescoSeeder(),
+            new DepartamentosSeeder(),
+            new MunicipiosSeeder(),
         ];
 
         $this->correrSeeders($listadoSeeders);
