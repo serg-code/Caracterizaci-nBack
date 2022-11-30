f828603071219df08a74b2ef4e211d69fbbbe893
_Sergiobg <sandresbg@hotmail.com>
2022-11-30 17:03:58 -0500

    creacion tabla induccion

---
+++ database/migrations/2022_11_30_165352_create_induccion_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('induccion', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->enum('curso_vida', ['Primera Infancia','Infancia','Adolescencia','Juventud','Adultez','Vejez','Preconcepcional','Maternoperinatal','Sin curso vida']);
+            $table->string('tipo_atencion');
+            $table->string('genero');
+            $table->string('edad_minima');
+            $table->string('edad_maxima');
+            $table->string('grupo_etario');
+            $table->string('frecuencia');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('induccion');
+    }
+};
