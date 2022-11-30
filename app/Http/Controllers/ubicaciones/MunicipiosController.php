8160408ccfe69ad0bb533dea137db8d24b4d1085
desteban <esteban28c@hotmail.com>
2022-11-30 15:03:46 -0500

    eliminar codigo comentado

--- app/Http/Controllers/ubicaciones/MunicipiosController.php
+++ app/Http/Controllers/ubicaciones/MunicipiosController.php
@@ -5,14 +5,12 @@ namespace App\Http\Controllers\ubicaciones;
 use App\Dev\RespuestaHttp;
 use App\Http\Controllers\Controller;
 use App\Models\Municipio;
-use Illuminate\Support\Facades\DB;
 
 class MunicipiosController extends Controller
 {
     public function mostrarMunicipiosDepartamento($codigoDepartamento)
     {
-        // $municipios = Municipio::where('cod_dpto', '=', $codigoDepartamento)->get();
-        $municipios = DB::table('municipios')->limit(2)->get();
+        $municipios = Municipio::where('cod_dpto', '=', $codigoDepartamento)->get();
 
         $respuesta = new RespuestaHttp();
         $respuesta->data = [
