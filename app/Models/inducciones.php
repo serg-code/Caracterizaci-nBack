febc1f7976531a6983e5ef42098c84f501d352c3
_Sergiobg <sandresbg@hotmail.com>
2022-12-01 15:04:08 -0500

    creacion migracion inducciones

--- app/Models/inducciones.php
+++ app/Models/inducciones.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class inducciones extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'primera infancia',
+        'infancia',
+        'adolescencia',
+        'juventud',
+        'adultez',
+        'vejez',
+        'preconcepcional',
+        'maternoperinatal',
+        'sin curso vida'
+    ];
+
+    protected $hidden = [
+        'deleted_at',
+    ];
+
+    public static function Guardarinducciones(array $datos)
+    {
+        $parentesco = new inducciones($datos);
+        $parentesco->save();
+    }
 }
