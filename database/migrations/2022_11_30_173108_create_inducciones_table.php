8d8db034710601874b9895e609fda960eb98f7e6
desteban <esteban28c@hotmail.com>
2022-11-30 21:38:36 -0500

    modificar migrasion de inducciones

--- database/migrations/2022_11_30_173108_create_inducciones_table.php
+++ database/migrations/2022_11_30_173108_create_inducciones_table.php
@@ -13,16 +13,26 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('inducciones', function (Blueprint $table) {
-            $table->id()->primary();
-            $table->enum('curso_vida', ['Primera Infancia','Infancia','Adolescencia','Juventud','Adultez','Vejez','Preconcepcional','Maternoperinatal','Sin curso vida']);
+        Schema::create('inducciones', function (Blueprint $table)
+        {
+            $table->id();
+            $table->enum('curso_vida', [
+                'Primera Infancia',
+                'Infancia',
+                'Adolescencia',
+                'Juventud',
+                'Adultez',
+                'Vejez',
+                'Preconcepcional',
+                'Maternoperinatal',
+                'Sin curso vida'
+            ]);
             $table->string('tipo_atencion');
             $table->string('genero');
             $table->string('edad_minima');
             $table->string('edad_maxima');
             $table->string('grupo_etario');
             $table->string('frecuencia');
-            $table->timestamps('deleted_at');
             $table->timestamps();
         });
     }
