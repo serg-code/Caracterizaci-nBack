cc094ad038db70ed8f82729371235bc20fb783b4
_Sergiobg <sandresbg@hotmail.com>
2022-12-06 09:29:34 -0500

    correcion  seeder opciones

--- database/seeders/Opciones/accidenteSeeder.php
+++ database/seeders/Opciones/accidenteSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders\Opciones;
 
 use App\Models\model\accidentes;
+use App\Models\Opcion;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,17 +16,17 @@ class accidenteseeder extends Seeder
      */
     public function run()
     {
-        $employee = new accidentes(["id" => "68", "ref_campo" => "¿Ha sufrido accidentes de transito?", "pregunta_opcion" => "no", "valor" => "0"]);
-        $employee = new accidentes(["id" => "69", "ref_campo" => "¿Ha sufrido accidentes de transito?", "pregunta_opcion" => "si", "valor" => "3"]);
-        $employee = new accidentes(["id" => "70", "ref_campo" => "Tipo de lesión", "pregunta_opcion" => "Sin lesión", "valor" => "0"]);
-        $employee = new accidentes(["id" => "71", "ref_campo" => "Tipo de lesión", "pregunta_opcion" => "Fractura", "valor" => "3"]);
-        $employee = new accidentes(["id" => "72", "ref_campo" => "Tipo de lesión", "pregunta_opcion" => "Trauma de tejido blando", "valor" => "3"]);
-        $employee = new accidentes(["id" => "73", "ref_campo" => "Tipo de lesión", "pregunta_opcion" => "Daño neurologíco", "valor" => "5"]);
-        $employee = new accidentes(["id" => "74", "ref_campo" => "Tipo de lesión", "pregunta_opcion" => "Discapacidad", "valor" => "10"]);
-        $employee = new accidentes(["id" => "75", "ref_campo" => "¿Ha sufrido accidentes laborales?", "pregunta_opcion" => "no", "valor" => "0"]);
-        $employee = new accidentes(["id" => "76", "ref_campo" => "¿Ha sufrido accidentes laborales?", "pregunta_opcion" => "si", "valor" => "3"]);
-        $employee = new accidentes(["id" => "77", "ref_campo" => "¿Recibe cuidados domiciliarios?", "pregunta_opcion" => "no", "valor" => "0"]);
-        $employee = new accidentes(["id" => "78", "ref_campo" => "¿Recibe cuidados domiciliarios?", "pregunta_opcion" => "si", "valor" => "3"]);
-        $employee = new accidentes(["id" => "79", "ref_campo" => "Diagnostico principal", "pregunta_opcion" => "texto", "valor" => "0"]);
+        Opcion::create(["id" => "68", "ref_campo" => "¿Ha sufrido accidentes de transito?", "pregunta_opcion" => "no", "valor" => "0"]);
+        Opcion::create(["id" => "69", "ref_campo" => "¿Ha sufrido accidentes de transito?", "pregunta_opcion" => "si", "valor" => "3"]);
+        Opcion::create(["id" => "70", "ref_campo" => "Tipo de lesión", "pregunta_opcion" => "Sin lesión", "valor" => "0"]);
+        Opcion::create(["id" => "71", "ref_campo" => "Tipo de lesión", "pregunta_opcion" => "Fractura", "valor" => "3"]);
+        Opcion::create(["id" => "72", "ref_campo" => "Tipo de lesión", "pregunta_opcion" => "Trauma de tejido blando", "valor" => "3"]);
+        Opcion::create(["id" => "73", "ref_campo" => "Tipo de lesión", "pregunta_opcion" => "Daño neurologíco", "valor" => "5"]);
+        Opcion::create(["id" => "74", "ref_campo" => "Tipo de lesión", "pregunta_opcion" => "Discapacidad", "valor" => "10"]);
+        Opcion::create(["id" => "75", "ref_campo" => "¿Ha sufrido accidentes laborales?", "pregunta_opcion" => "no", "valor" => "0"]);
+        Opcion::create(["id" => "76", "ref_campo" => "¿Ha sufrido accidentes laborales?", "pregunta_opcion" => "si", "valor" => "3"]);
+        Opcion::create(["id" => "77", "ref_campo" => "¿Recibe cuidados domiciliarios?", "pregunta_opcion" => "no", "valor" => "0"]);
+        Opcion::create(["id" => "78", "ref_campo" => "¿Recibe cuidados domiciliarios?", "pregunta_opcion" => "si", "valor" => "3"]);
+        Opcion::create(["id" => "79", "ref_campo" => "Diagnostico principal", "pregunta_opcion" => "texto", "valor" => "0"]);
     }
 }
--- database/seeders/Opciones/cuidados_domiciliariosSeeder.php
+++ database/seeders/Opciones/cuidados_domiciliariosSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders\Opciones;
 
 use App\Models\model\cuidados_domiciliarios;
+use App\Models\Opcion;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,17 +16,17 @@ class cuidados_domiciliariosSeeder extends Seeder
      */
     public function run()
     {
-        $employee = new cuidados_domiciliarios(["id" => "79", "ref_campo" => "diagnostico_principal", "pregunta_opcion" => "texto", "valor" => "0"]);
-        $employee = new cuidados_domiciliarios(["id" => "80", "ref_campo" => "Causa", "pregunta_opcion" => "Domiciliarío cronico", "valor" => "0"]);
-        $employee = new cuidados_domiciliarios(["id" => "81", "ref_campo" => "Causa", "pregunta_opcion" => "Domiciliarío agudo", "valor" => "0"]);
-        $employee = new cuidados_domiciliarios(["id" => "82", "ref_campo" => "Causa", "pregunta_opcion" => "Domiciliarío ventilado", "valor" => "0"]);
-        $employee = new cuidados_domiciliarios(["id" => "83", "ref_campo" => "Fecha de inicio del cuidado domiciliario", "pregunta_opcion" => "texto", "valor" => "0"]);
-        $employee = new cuidados_domiciliarios(["id" => "84", "ref_campo" => "¿Recibe oxigeno domiciliario?", "pregunta_opcion" => "no", "valor" => "1"]);
-        $employee = new cuidados_domiciliarios(["id" => "85", "ref_campo" => "¿Recibe oxigeno domiciliario?", "pregunta_opcion" => "si", "valor" => "3"]);
-        $employee = new cuidados_domiciliarios(["id" => "86", "ref_campo" => "Plan aprobado", "pregunta_opcion" => "Estándar", "valor" => "0"]);
-        $employee = new cuidados_domiciliarios(["id" => "87", "ref_campo" => "Plan aprobado", "pregunta_opcion" => "Alto flujo", "valor" => "0"]);
-        $employee = new cuidados_domiciliarios(["id" => "88", "ref_campo" => "Plan aprobado", "pregunta_opcion" => "Bajo flujo", "valor" => "0"]);
-        $employee = new cuidados_domiciliarios(["id" => "89", "ref_campo" => "Plan aprobado", "pregunta_opcion" => "Concentrador", "valor" => "0"]);
-        $employee = new cuidados_domiciliarios(["id" => "90", "ref_campo" => "Plan aprobado", "pregunta_opcion" => "Mixto", "valor" => "0"]);
+        Opcion::create(["id" => "79", "ref_campo" => "diagnostico_principal", "pregunta_opcion" => "texto", "valor" => "0"]);
+        Opcion::create(["id" => "80", "ref_campo" => "Causa", "pregunta_opcion" => "Domiciliarío cronico", "valor" => "0"]);
+        Opcion::create(["id" => "81", "ref_campo" => "Causa", "pregunta_opcion" => "Domiciliarío agudo", "valor" => "0"]);
+        Opcion::create(["id" => "82", "ref_campo" => "Causa", "pregunta_opcion" => "Domiciliarío ventilado", "valor" => "0"]);
+        Opcion::create(["id" => "83", "ref_campo" => "Fecha de inicio del cuidado domiciliario", "pregunta_opcion" => "texto", "valor" => "0"]);
+        Opcion::create(["id" => "84", "ref_campo" => "¿Recibe oxigeno domiciliario?", "pregunta_opcion" => "no", "valor" => "1"]);
+        Opcion::create(["id" => "85", "ref_campo" => "¿Recibe oxigeno domiciliario?", "pregunta_opcion" => "si", "valor" => "3"]);
+        Opcion::create(["id" => "86", "ref_campo" => "Plan aprobado", "pregunta_opcion" => "Estándar", "valor" => "0"]);
+        Opcion::create(["id" => "87", "ref_campo" => "Plan aprobado", "pregunta_opcion" => "Alto flujo", "valor" => "0"]);
+        Opcion::create(["id" => "88", "ref_campo" => "Plan aprobado", "pregunta_opcion" => "Bajo flujo", "valor" => "0"]);
+        Opcion::create(["id" => "89", "ref_campo" => "Plan aprobado", "pregunta_opcion" => "Concentrador", "valor" => "0"]);
+        Opcion::create(["id" => "90", "ref_campo" => "Plan aprobado", "pregunta_opcion" => "Mixto", "valor" => "0"]);
     }
 }
