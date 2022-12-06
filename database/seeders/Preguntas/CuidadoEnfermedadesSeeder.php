33ed227a06d8cf1d7fd448486b6baa90adbb1034
desteban <esteban28c@hotmail.com>
2022-12-06 09:20:44 -0500

    agregar seeders de preguntas de los integrantes

---
+++ database/seeders/Preguntas/CuidadoEnfermedadesSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders\Preguntas;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CuidadoEnfermedadesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
