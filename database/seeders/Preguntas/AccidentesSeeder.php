a790082d06f64d53d041496a8a82e6b943f25f6e
desteban <esteban28c@hotmail.com>
2022-12-06 10:49:03 -0500

    arreglos en el ref_seccion

--- database/seeders/Preguntas/AccidentesSeeder.php
+++ database/seeders/Preguntas/AccidentesSeeder.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Database\Seeders;
+namespace Database\Seeders\Preguntas;
 
 use App\Models\Pregunta;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -15,10 +15,8 @@ class AccidentesSeeder extends Seeder
      */
     public function run()
     {
-        Pregunta::create(["ref_campo"=> "accidentes_transito", "ref_seccion" => "accidentes", "descripcion" => "¿Ha sufrido accidentes de transito?", "tipo" => "seleccion"]);
-        Pregunta::create(["ref_campo"=> "tipo_lesión", "ref_seccion" => "accidentes", "descripcion" => "Tipo de lesión", "tipo" => "seleccion multiple"]);	
-Pregunta::create(["ref_campo"=> "accidentes_laborales", "ref_seccion" => "accidentes", "descripcion" => "¿Ha sufrido accidentes laborales?", "tipo" => "seleccion"]);	
-
-
+        Pregunta::create(["ref_campo" => "accidentes_transito", "ref_seccion" => "accidentes", "descripcion" => "¿Ha sufrido accidentes de transito?", "tipo" => "seleccion"]);
+        Pregunta::create(["ref_campo" => "tipo_lesión", "ref_seccion" => "accidentes", "descripcion" => "Tipo de lesión", "tipo" => "seleccion multiple"]);
+        Pregunta::create(["ref_campo" => "accidentes_laborales", "ref_seccion" => "accidentes", "descripcion" => "¿Ha sufrido accidentes laborales?", "tipo" => "seleccion"]);
     }
 }
