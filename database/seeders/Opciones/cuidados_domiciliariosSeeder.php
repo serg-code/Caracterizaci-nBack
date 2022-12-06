a6ac577d2a194a15b4b4ebfe4427da947c2660c6
_Sergiobg <sandresbg@hotmail.com>
2022-12-06 10:58:03 -0500

    correcion secciones

--- database/seeders/Opciones/cuidados_domiciliariosSeeder.php
+++ database/seeders/Opciones/cuidados_domiciliariosSeeder.php
@@ -22,13 +22,13 @@ class cuidados_domiciliariosSeeder extends Seeder
         Opcion::create(["id" => "80", "ref_campo" => "Causa", "pregunta_opcion" => "Domiciliarío cronico", "valor" => "0"]);
         Opcion::create(["id" => "81", "ref_campo" => "Causa", "pregunta_opcion" => "Domiciliarío agudo", "valor" => "0"]);
         Opcion::create(["id" => "82", "ref_campo" => "Causa", "pregunta_opcion" => "Domiciliarío ventilado", "valor" => "0"]);
-        Opcion::create(["id" => "83", "ref_campo" => "Fecha de inicio del cuidado domiciliario", "pregunta_opcion" => "texto", "valor" => "0"]);
-        Opcion::create(["id" => "84", "ref_campo" => "¿Recibe oxigeno domiciliario?", "pregunta_opcion" => "no", "valor" => "1"]);
-        Opcion::create(["id" => "85", "ref_campo" => "¿Recibe oxigeno domiciliario?", "pregunta_opcion" => "si", "valor" => "3"]);
-        Opcion::create(["id" => "86", "ref_campo" => "Plan aprobado", "pregunta_opcion" => "Estándar", "valor" => "0"]);
-        Opcion::create(["id" => "87", "ref_campo" => "Plan aprobado", "pregunta_opcion" => "Alto flujo", "valor" => "0"]);
-        Opcion::create(["id" => "88", "ref_campo" => "Plan aprobado", "pregunta_opcion" => "Bajo flujo", "valor" => "0"]);
-        Opcion::create(["id" => "89", "ref_campo" => "Plan aprobado", "pregunta_opcion" => "Concentrador", "valor" => "0"]);
-        Opcion::create(["id" => "90", "ref_campo" => "Plan aprobado", "pregunta_opcion" => "Mixto", "valor" => "0"]);
+        Opcion::create(["id" => "83", "ref_campo" => "fecha_inicio_domiciliario", "pregunta_opcion" => "texto", "valor" => "0"]);
+        Opcion::create(["id" => "84", "ref_campo" => "oxigeno_domiciliario", "pregunta_opcion" => "no", "valor" => "1"]);
+        Opcion::create(["id" => "85", "ref_campo" => "oxigeno_domiciliario", "pregunta_opcion" => "si", "valor" => "3"]);
+        Opcion::create(["id" => "86", "ref_campo" => "plan_aprobado", "pregunta_opcion" => "Estándar", "valor" => "0"]);
+        Opcion::create(["id" => "87", "ref_campo" => "plan_aprobado", "pregunta_opcion" => "Alto flujo", "valor" => "0"]);
+        Opcion::create(["id" => "88", "ref_campo" => "plan_aprobado", "pregunta_opcion" => "Bajo flujo", "valor" => "0"]);
+        Opcion::create(["id" => "89", "ref_campo" => "plan_aprobado", "pregunta_opcion" => "Concentrador", "valor" => "0"]);
+        Opcion::create(["id" => "90", "ref_campo" => "plan_aprobado", "pregunta_opcion" => "Mixto", "valor" => "0"]);
     }
 }
