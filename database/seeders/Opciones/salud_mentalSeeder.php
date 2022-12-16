85c07bc4e194aa4f2f03621a683a3386af90e02b
_Sergiobg <sandresbg@hotmail.com>
2022-12-16 11:35:36 -0500

    Seeders salud mental y salud

---
+++ database/seeders/Opciones/salud_mentalSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Database\Seeders\Opciones;
+
+use App\Models\model\salud_mental;
+use App\Models\Opcion;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class salud_mentalSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Opcion::create(["id"=> "92", "ref_campo" =>  "depresión", "pregunta_opcion" => "SI", "valor" => "1"]);
+        Opcion::create(["id"=> "92", "ref_campo" =>  "depresión", "pregunta_opcion" => "SI", "valor" => "3"]);
+        Opcion::create(["id"=> "93", "ref_campo" =>  "intentosuicidio", "pregunta_opcion" => "NO", "valor" => "1"]);
+        Opcion::create(["id"=> "94", "ref_campo" =>  "intentosuicidio", "pregunta_opcion" => "SI", "valor" => "3"]);
+        Opcion::create(["id"=> "95", "ref_campo" =>  "esquizofrenia", "pregunta_opcion" => "NO", "valor" => "1"]);
+        Opcion::create(["id"=> "96", "ref_campo" =>  "esquizofrenia", "pregunta_opcion" => "SI", "valor" => "3"]);
+        Opcion::create(["id"=> "97", "ref_campo" =>  "trastorno_afectivo", "pregunta_opcion" => "NO", "valor" => "1"]);
+        Opcion::create(["id"=> "98", "ref_campo" =>  "trastorno_afectivo", "pregunta_opcion" => "SI", "valor" => "3"]);
+        Opcion::create(["id"=> "99", "ref_campo" =>  "bulimia", "pregunta_opcion" => "NO", "valor" => "1"]);
+        Opcion::create(["id"=> "100", "ref_campo" =>  "bulimia", "pregunta_opcion" => "SI", "valor" => "3"]);
+        Opcion::create(["id"=> "101", "ref_campo" =>  "anorexia", "pregunta_opcion" => "NO", "valor" => "1"]);
+        Opcion::create(["id"=> "102", "ref_campo" =>  "anorexia", "pregunta_opcion" => "SI", "valor" => "3"]);
+        Opcion::create(["id"=> "103", "ref_campo" =>  "tratamiento", "pregunta_opcion" => "NO", "valor" => "1"]);
+        Opcion::create(["id"=> "104", "ref_campo" =>  "tratamiento", "pregunta_opcion" => "SI", "valor" => "3"]);
+        Opcion::create(["id"=> "105", "ref_campo" =>  "diagnóstico", "pregunta_opcion" => "diagnostico", "valor" => "0"]);
+        Opcion::create(["id"=> "106", "ref_campo" =>  "violencia_fisica", "pregunta_opcion" => "NO", "valor" => "1"]);
+        Opcion::create(["id"=> "107", "ref_campo" =>  "violencia_fisica", "pregunta_opcion" => "SI", "valor" => "3"]);
+        Opcion::create(["id"=> "108", "ref_campo" =>  "violencia_psicologíca", "pregunta_opcion" => "NO", "valor" => "1"]);
+        Opcion::create(["id"=> "109", "ref_campo" =>  "violencia_psicologíca", "pregunta_opcion" => "SI", "valor" => "3"]);
+        Opcion::create(["id"=> "110", "ref_campo" =>  "violencia_sexual", "pregunta_opcion" => "NO", "valor" => "1"]);
+        Opcion::create(["id"=> "111", "ref_campo" =>  "violencia_sexual", "pregunta_opcion" => "SI", "valor" => "3"]);
+        Opcion::create(["id"=> "112", "ref_campo" =>  "violencia_institucional", "pregunta_opcion" => "NO", "valor" => "1"]);
+        Opcion::create(["id"=> "113", "ref_campo" =>  "violencia_institucional", "pregunta_opcion" => "SI", "valor" => "3"]);
+        Opcion::create(["id"=> "114", "ref_campo" =>  "violencia_social", "pregunta_opcion" => "NO", "valor" => "1"]);
+        Opcion::create(["id"=> "115", "ref_campo" =>  "violencia_social", "pregunta_opcion" => "SI", "valor" => "3"]);
+        Opcion::create(["id"=> "116", "ref_campo" =>  "violencia_gestacion", "pregunta_opcion" => "NO", "valor" => "1"]);
+        Opcion::create(["id"=> "117", "ref_campo" =>  "violencia_gestacion", "pregunta_opcion" => "SI", "valor" => "3"]);
+        
+        }
+        }
+    
+
+
---
+++ database/seeders/Preguntas/SaludMentalSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders\Pregunta;
+
+use App\Models\Pregunta;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SaludMentalSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Pregunta::create(["ref_campo"=> "depresion", "ref_seccion" => "salud_mental ", "descripcion" => "Intento de suicidio", "tipo" => "selección"]);
+        Pregunta::create(["ref_campo"=> "intento_suicidio", "ref_seccion" => "salud_mental ", "descripcion" => "Intento de suicidio", "tipo" => "selección"]);
+        Pregunta::create(["ref_campo"=> "trastorno_afectivo", "ref_seccion" => "salud_mental ", "descripcion" => "Trastorno afectivo", "tipo" => "selección"]);
+        Pregunta::create(["ref_campo"=> "bulimia", "ref_seccion" => "salud_mental ", "descripcion" => "Bulímia", "tipo" => "selección"]);
+        Pregunta::create(["ref_campo"=> "anorexia", "ref_seccion" => "salud_mental ", "descripcion" => "Anorexía", "tipo" => "selección"]);
+        Pregunta::create(["ref_campo"=> "tratamiento", "ref_seccion" => "salud_mental ", "descripcion" => "¿Esta en tratamiento?", "tipo" => "selección"]);
+        Pregunta::create(["ref_campo"=> "diagnostico", "ref_seccion" => "salud_mental ", "descripcion" => "Diagnóstico de salud mental", "tipo" => "texto"]);
+        Pregunta::create(["ref_campo"=> "violencia_fisica", "ref_seccion" => "salud_mental ", "descripcion" => "Violencia física", "tipo" => "selección"]);
+        Pregunta::create(["ref_campo"=> "violencia_psicologica", "ref_seccion" => "salud_mental ", "descripcion" => "Violencia Psicologíca", "tipo" => "selección"]);
+        Pregunta::create(["ref_campo"=> "violencia_sexual", "ref_seccion" => "salud_mental ", "descripcion" => "Violencia sexual", "tipo" => "selección"]);
+        Pregunta::create(["ref_campo"=> "violencia_institucional", "ref_seccion" => "salud_mental ", "descripcion" => "Violencia institucional", "tipo" => "selección"]);
+        Pregunta::create(["ref_campo"=> "violencia_social", "ref_seccion" => "salud_mental ", "descripcion" => "Violencia social", "tipo" => "selección"]);
+        Pregunta::create(["ref_campo"=> "violencia_gestacion", "ref_seccion" => "salud_mental ", "descripcion" => "Violencia en la gestacion", "tipo" => "selección"]);
+            }
+        }
