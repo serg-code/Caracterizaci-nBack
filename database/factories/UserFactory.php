de907ef342e556a4a06fd87917b19cfafd28fbdf
desteban <esteban28c@hotmail.com>
2022-12-21 16:22:29 -0500

    modificar factory de usuarios

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,6 +23,9 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             // 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'password' => 'password',
+            'tipo_identificacion' => 'CC',
+            'identificacion' => fake()->numberBetween(1000000000),
+            'telefono' => fake()->numberBetween(3000000000, 3999999999),
             'remember_token' => Str::random(10),
         ];
     }
