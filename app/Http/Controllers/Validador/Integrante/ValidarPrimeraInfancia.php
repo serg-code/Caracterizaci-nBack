87e501eca23d89b2129f5b19890233b840a234f2
desteban <esteban28c@hotmail.com>
2023-01-12 23:06:24 -0500

    validaciones en primera infancia

--- app/Http/Controllers/Validador/Integrante/ValidarPrimeraInfancia.php
+++ app/Http/Controllers/Validador/Integrante/ValidarPrimeraInfancia.php
@@ -8,4 +8,50 @@ use Illuminate\Http\Request;
 class ValidarPrimeraInfancia extends Controller
 {
     //
+    public function __construct(
+        protected array $seccion = [],
+        protected int $puntaje = 0,
+        protected int $edad = 0,
+    )
+    {
+    }
+
+    public function validar()
+    {
+        $this->PesoNacer();
+        $this->PesoNacer();
+        $this->TallaNacer();
+    }
+
+    protected function PesoNacer()
+    {
+        if ($this->seccion['pi_peso_al_nacer'] > 2800)
+        {
+            $this->puntaje += 1;
+        }
+
+        if ($this->seccion['pi_peso_al_nacer'] > 4000)
+        {
+            $this->puntaje += 5;
+        }
+    }
+
+    protected function PesoActual()
+    {
+        //* Condicional con peso y talla
+        //pi_peso_actual
+    }
+
+    protected function TallaNacer()
+    {
+        if ($this->seccion['pi_talla_al_nacer'] < 40)
+        {
+            $this->puntaje += 5;
+        }
+
+        if ($this->seccion['pi_talla_al_nacer'] > 55)
+        {
+            $this->puntaje += 5;
+        }
+    }
 }
