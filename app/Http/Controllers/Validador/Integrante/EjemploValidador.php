084d7b98bfe7b6d761450d1755d7269c7190649f
desteban <esteban28c@hotmail.com>
2023-01-19 10:47:00 -0500

    actualizar ejemplo de validador del integrante

--- app/Http/Controllers/Validador/Integrante/EjemploValidador.php
+++ app/Http/Controllers/Validador/Integrante/EjemploValidador.php
@@ -2,100 +2,21 @@
 
 namespace App\Http\Controllers\Validador\Integrante;
 
-use App\Dev\Encuesta\OpcionPregunta;
-use App\Http\Controllers\Controller;
 use App\Interfaces\ValidacionEncuesta;
 use App\Models\Integrantes;
-use App\Models\Opcion;
-use Carbon\Carbon;
-use Illuminate\Http\Request;
 
-class EjemploValidador extends Controller implements ValidacionEncuesta
+class EjemploValidador extends ValidacionIntegrante implements ValidacionEncuesta
 {
-
-    protected array $errores;
-    protected int $puntaje;
-    protected int $edad;
-    protected int $mesesEdad;
-    protected array $seccionValidada;
 
     public function __construct(
         protected Integrantes $integrante = new Integrantes(),
         protected array $seccion = [],
     )
     {
-        $this->puntaje = 0;
-        $this->errores = [];
-        $this->seccionValidada = [];
-
-        $fechaNacimiento = Carbon::createFromFormat('Y-m-d', $this->integrante->fecha_nacimiento);
-        $fechaActual = Carbon::now();
-        $diferenciaFechas = $fechaActual->diff($fechaNacimiento);
-        $this->edad = $diferenciaFechas->y;
-        $this->mesesEdad = $diferenciaFechas->format("%m");
+        parent::__construct('adolescencia', $integrante, $seccion);
     }
 
     public function validar()
     {
-        $edad = $this->edad;
-        if ($edad < 10 || $edad > 70)
-        {
-            return false;
-        }
-
-        //validaciones del excel
-    }
-
-    public function obtenerErrores(): array
-    {
-        return $this->errores;
-    }
-
-    public function obtenerPuntaje(): int
-    {
-        return $this->puntaje;
-    }
-
-    public function obtenerPreguntas(): array
-    {
-        return [];
-    }
-
-    public function obtenerSeccion(): array
-    {
-        return $this->seccionValidada;
-    }
-
-    protected function puntuacion(string $refCampo): Opcion
-    {
-        $respuestaEncuesta = $this->seccion[$refCampo] ?? null;
-        if (empty($respuestaEncuesta))
-        {
-            array_push($this->errores, [$refCampo => 'No encontramos la pregunta ' . $refCampo]);
-            return new Opcion(['id' => 0, 'valor' => 0]);
-        }
-
-        $opcion = OpcionPregunta::opcionPregunta($refCampo, $respuestaEncuesta);
-        if (empty($opcion))
-        {
-            array_push($this->errores, [
-                $refCampo => $respuestaEncuesta . " no es un respuesta valida para $refCampo"
-            ]);
-            return new Opcion(['id' => 0, 'valor' => 0]);
-        }
-
-        array_push($this->seccionValidada, [$refCampo => $this->seccion[$refCampo]]);
-        $this->puntaje += $opcion->valor;
-        return $opcion;
-    }
-
-    protected function validacionSimple(string $refCampo, bool $validar): Opcion
-    {
-        if ($validar)
-        {
-            return $this->puntuacion($refCampo);
-        }
-
-        return new Opcion(['id' => 0, 'valor' => 0]);
     }
 }
