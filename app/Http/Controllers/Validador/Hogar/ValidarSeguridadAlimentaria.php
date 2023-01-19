197bec4592fa12246c6cabf060001d4bf116f7bf
Sergio Blanco <sandresbg@hotmail.com>
2023-01-19 15:46:52 -0500

    validador seguridad alimentaria

--- app/Http/Controllers/Validador/Hogar/ValidarSeguridadAlimentaria.php
+++ app/Http/Controllers/Validador/Hogar/ValidarSeguridadAlimentaria.php
@@ -41,4 +41,131 @@ class ValidarSeguridadAlimentaria extends ValidacionHogar implements ValidacionE
             $this->sumarPuntaje(5);
         }
     }
+
+
+    protected function consumoHuevos()
+    {
+        $vecesComeCarne = $this->seccion['consume_cerdo_res_pollo'];
+        if ($vecesComeCarne < 0)
+        {
+            $this->agregarErrror('consume_cerdo_res_pollo', "($vecesComeCarne) no es valido como respuesta");
+        }
+
+        if ($vecesComeCarne >= 0 && $vecesComeCarne < 3)
+        {
+            $this->sumarPuntaje(5);
+        }
+    }
+
+    protected function consumoFrijolLentejas()
+    {
+        $vecesComeCarne = $this->seccion['consume_cerdo_res_pollo'];
+        if ($vecesComeCarne < 0)
+        {
+            $this->agregarErrror('consume_cerdo_res_pollo', "($vecesComeCarne) no es valido como respuesta");
+        }
+
+        if ($vecesComeCarne >= 0 && $vecesComeCarne < 3)
+        {
+            $this->sumarPuntaje(5);
+        }
+    }
+
+    protected function consumoLacteos()
+    {
+        $vecesComeCarne = $this->seccion['consume_cerdo_res_pollo'];
+        if ($vecesComeCarne < 0)
+        {
+            $this->agregarErrror('consume_cerdo_res_pollo', "($vecesComeCarne) no es valido como respuesta");
+        }
+
+        if ($vecesComeCarne >= 0 && $vecesComeCarne < 3)
+        {
+            $this->sumarPuntaje(5);
+        }
+    }
+
+    protected function consumoHarinas()
+    {
+        $vecesComeCarne = $this->seccion['consume_cerdo_res_pollo'];
+        if ($vecesComeCarne < 0)
+        {
+            $this->agregarErrror('consume_cerdo_res_pollo', "($vecesComeCarne) no es valido como respuesta");
+        }
+
+        if ($vecesComeCarne >= 0 && $vecesComeCarne > 4)
+        {
+            $this->sumarPuntaje(5);
+        }
+    }
+
+    protected function consumoVerduras()
+    {
+        $vecesComeCarne = $this->seccion['consume_cerdo_res_pollo'];
+        if ($vecesComeCarne < 0)
+        {
+            $this->agregarErrror('consume_cerdo_res_pollo', "($vecesComeCarne) no es valido como respuesta");
+        }
+
+        if ($vecesComeCarne >= 0 && $vecesComeCarne < 3)
+        {
+            $this->sumarPuntaje(5);
+        }
+    }
+
+    protected function consumoFrutasFrescas()
+    {
+        $vecesComeCarne = $this->seccion['consume_cerdo_res_pollo'];
+        if ($vecesComeCarne < 0)
+        {
+            $this->agregarErrror('consume_cerdo_res_pollo', "($vecesComeCarne) no es valido como respuesta");
+        }
+
+        if ($vecesComeCarne >= 0 && $vecesComeCarne < 3)
+        {
+            $this->sumarPuntaje(5);
+        }
+    }
+
+    protected function consumoEnlatados()
+    {
+        $vecesComeCarne = $this->seccion['consume_cerdo_res_pollo'];
+        if ($vecesComeCarne < 0)
+        {
+            $this->agregarErrror('consume_cerdo_res_pollo', "($vecesComeCarne) no es valido como respuesta");
+        }
+
+        if ($vecesComeCarne >= 0 && $vecesComeCarne < 3)
+        {
+            $this->sumarPuntaje(5);
+        }
+    }
+
+    protected function consumoPlatanoYuca()
+    {
+        $vecesComeCarne = $this->seccion['consume_cerdo_res_pollo'];
+        if ($vecesComeCarne < 0)
+        {
+            $this->agregarErrror('consume_cerdo_res_pollo', "($vecesComeCarne) no es valido como respuesta");
+        }
+
+        if ($vecesComeCarne >= 0 && $vecesComeCarne < 3)
+        {
+            $this->sumarPuntaje(5);
+        }
+    }
+
+    protected function consumoGaseosa()
+    {
+        $vecesComeCarne = $this->seccion['consume_cerdo_res_pollo'];
+        if ($vecesComeCarne < 0)
+        {
+            $this->agregarErrror('consume_cerdo_res_pollo', "($vecesComeCarne) no es valido como respuesta");
+        }
+
+        if ($vecesComeCarne >= 0 && $vecesComeCarne < 2)
+        {
+            $this->sumarPuntaje(5);
+        }
+    }
 }
