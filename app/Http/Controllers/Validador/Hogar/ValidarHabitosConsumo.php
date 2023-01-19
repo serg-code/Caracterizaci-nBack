57020d67e0c4a931527c9f9a391e24c59441e804
desteban <esteban28c@hotmail.com>
2023-01-19 10:58:46 -0500

    implementar la clase ValidacionHogar

--- app/Http/Controllers/Validador/Hogar/ValidarHabitosConsumo.php
+++ app/Http/Controllers/Validador/Hogar/ValidarHabitosConsumo.php
@@ -2,28 +2,14 @@
 
 namespace App\Http\Controllers\Validador\Hogar;
 
-use App\Dev\Encuesta\OpcionPregunta;
-use App\Http\Controllers\Controller;
 use App\Interfaces\ValidacionEncuesta;
-use App\Models\Hogar\Hogar;
-use App\Models\Opcion;
-use Illuminate\Http\Request;
 
-class ValidarHabitosConsumo implements ValidacionEncuesta
+class ValidarHabitosConsumo extends ValidacionHogar implements ValidacionEncuesta
 {
 
-    protected array $errores;
-    protected int $puntaje;
-    protected array $seccionValidada;
-
-    public function __construct(
-        protected Hogar $hogar = new Hogar(),
-        protected array $seccion = [],
-    )
+    public function __construct(array $seccion)
     {
-        $this->puntaje = 0;
-        $this->errores = [];
-        $this->seccionValidada = [];
+        parent::__construct('habitos_consumo', $seccion);
     }
 
     public function validar()
@@ -33,58 +19,5 @@ class ValidarHabitosConsumo implements ValidacionEncuesta
         $this->puntuacion('hierve_leche');
         $this->puntuacion('lavar_frutas_verduras');
         $this->puntuacion('alimentos_crudos_separados_cocidos');
-    }
-
-    public function obtenerErrores(): array
-    {
-        return $this->errores;
-    }
-
-    public function obtenerPuntaje(): int
-    {
-        return $this->puntaje;
-    }
-
-    public function obtenerPreguntas(): array
-    {
-        return [];
-    }
-
-    public function obtenerSeccion(): array
-    {
-        return $this->seccionValidada;
-    }
-
-    protected function puntuacion(string $refCampo): Opcion
-    {
-        $respuestaEncuesta = $this->seccion[$refCampo] ?? null;
-        if (empty($respuestaEncuesta))
-        {
-            array_push($this->errores, [$refCampo => 'No encontramos la pregunta ' . $refCampo]);
-            return new Opcion(['id' => 0, 'valor' => 0]);
-        }
-
-        $opcion = OpcionPregunta::opcionPregunta($refCampo, $respuestaEncuesta);
-        if (empty($opcion))
-        {
-            array_push($this->errores, [
-                $refCampo => $respuestaEncuesta . " no es un respuesta valida para $refCampo"
-            ]);
-            return new Opcion(['id' => 0, 'valor' => 0]);
-        }
-
-        array_push($this->seccionValidada, [$refCampo => $this->seccion[$refCampo]]);
-        $this->puntaje += $opcion->valor;
-        return $opcion;
-    }
-
-    protected function validacionSimple(string $refCampo, bool $validar): ?Opcion
-    {
-        if ($validar)
-        {
-            return $this->puntuacion($refCampo);
-        }
-
-        return null;
     }
 }
--- app/Http/Controllers/Validador/Hogar/ValidarVivienda.php
+++ app/Http/Controllers/Validador/Hogar/ValidarVivienda.php
@@ -2,29 +2,15 @@
 
 namespace App\Http\Controllers\Validador\Hogar;
 
-use App\Dev\Encuesta\OpcionPregunta;
-use App\Http\Controllers\Controller;
 use App\Interfaces\ValidacionEncuesta;
-use App\Models\Hogar\Hogar;
-use App\Models\Opcion;
 use App\Models\Secciones\Hogar\CIUU;
-use Illuminate\Http\Request;
 
-class ValidarVivienda implements ValidacionEncuesta
+class ValidarVivienda extends ValidacionHogar implements ValidacionEncuesta
 {
 
-    protected array $errores;
-    protected int $puntaje;
-    protected array $seccionValidada;
-
-    public function __construct(
-        protected Hogar $hogar = new Hogar(),
-        protected array $seccion = [],
-    )
+    public function __construct(array $seccion)
     {
-        $this->puntaje = 0;
-        $this->errores = [];
-        $this->seccionValidada = [];
+        parent::__construct('vivienda', $seccion);
     }
 
     public function validar()
@@ -50,59 +36,6 @@ class ValidarVivienda implements ValidacionEncuesta
         $this->puntuacion('tipos_material_piso');
         $this->puntuacion('tipos_material_techo');
         $this->puntuacion('tipos_material_paredes');
-    }
-
-    public function obtenerErrores(): array
-    {
-        return $this->errores;
-    }
-
-    public function obtenerPuntaje(): int
-    {
-        return $this->puntaje;
-    }
-
-    public function obtenerPreguntas(): array
-    {
-        return [];
-    }
-
-    public function obtenerSeccion(): array
-    {
-        return $this->seccionValidada;
-    }
-
-    protected function puntuacion(string $refCampo): Opcion
-    {
-        $respuestaEncuesta = $this->seccion[$refCampo] ?? null;
-        if (empty($respuestaEncuesta))
-        {
-            array_push($this->errores, [$refCampo => 'No encontramos la pregunta ' . $refCampo]);
-            return new Opcion(['id' => 0, 'valor' => 0]);
-        }
-
-        $opcion = OpcionPregunta::opcionPregunta($refCampo, $respuestaEncuesta);
-        if (empty($opcion))
-        {
-            array_push($this->errores, [
-                $refCampo => $respuestaEncuesta . " no es un respuesta valida para $refCampo"
-            ]);
-            return new Opcion(['id' => 0, 'valor' => 0]);
-        }
-
-        array_push($this->seccionValidada, [$refCampo => $this->seccion[$refCampo]]);
-        $this->puntaje += $opcion->valor;
-        return $opcion;
-    }
-
-    protected function validacionSimple(string $refCampo, bool $validar): ?Opcion
-    {
-        if ($validar)
-        {
-            return $this->puntuacion($refCampo);
-        }
-
-        return null;
     }
 
     protected function activiadadProductiva()
