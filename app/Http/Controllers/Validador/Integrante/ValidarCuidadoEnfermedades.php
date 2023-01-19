427ec62990b9bbabd6f006ce30c46fe917201d10
desteban <esteban28c@hotmail.com>
2023-01-19 17:01:37 -0500

    revisar preguntas cuidado enfermedades

--- app/Http/Controllers/Validador/Integrante/ValidarCuidadoEnfermedades.php
+++ app/Http/Controllers/Validador/Integrante/ValidarCuidadoEnfermedades.php
@@ -18,27 +18,29 @@ class ValidarCuidadoEnfermedades extends ValidacionIntegrante implements Validac
 
     public function validar()
     {
-        $this->puntuacion('actividad_fisica');
-        $this->puntuacion('artritis_remautidea');
         $this->puntuacion('cancer');
+        $this->puntuacion('artritis_remautidea');
+        $this->puntuacion('vih_sida');
+        $this->puntuacion('hemofilia');
+        $this->puntuacion('insuficiencia_renal');
+        $this->puntuacion('fuma');
+        $this->puntuacion('actividad_fisica');
+        $this->puntuacion('vacuna_fiebre_amarilla');
         $this->puntuacion('diabetes');
         $this->puntuacion('diabetes_trimestral');
-        $this->puntuacion('fuma');
-        $this->puntuacion('hemofilia');
-        $this->puntuacion('hemoglobina_glococilada');
         $this->puntuacion('hipertencion_trimestral');
-        $this->puntuacion('insuficiencia_renal');
-        $this->puntuacion('tension_diastolica');
         $this->puntuacion('tension_sistolica');
-        $this->puntuacion('vacuna_fiebre_amarilla');
-        $this->puntuacion('vih_sida');
-        $this->puntuacion('ha_estado_embarazada');
-        $this->puntuacion('cuantos_embarazos_ha_tenido');
-        $this->puntuacion('hijos_muertos_parto_natural');
-        $this->puntuacion('hijos_vivos_parto_natural');
-        $this->puntuacion('hijos_muertos_por_cesarea');
-        $this->puntuacion('hijos_vivos_por_cesarea');
-        $this->puntuacion('cuantos_abortos');
-        $this->puntuacion('cuantos_gemelos_multiples');
+        $this->puntuacion('tension_diastolica');
+        $this->puntuacion('hemoglobina_glococilada');
+        $this->puntuacion('enfermedades_costosas');
+
+        // $this->puntuacion('ha_estado_embarazada');
+        // $this->puntuacion('cuantos_embarazos_ha_tenido');
+        // $this->puntuacion('hijos_muertos_parto_natural');
+        // $this->puntuacion('hijos_vivos_parto_natural');
+        // $this->puntuacion('hijos_muertos_por_cesarea');
+        // $this->puntuacion('hijos_vivos_por_cesarea');
+        // $this->puntuacion('cuantos_abortos');
+        // $this->puntuacion('cuantos_gemelos_multiples');
     }
 }
