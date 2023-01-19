b44fc2a61ba0e79dcbd3ddab0ff335bdc7154634
desteban <esteban28c@hotmail.com>
2023-01-19 15:25:34 -0500

    validar fallecidos

--- app/Http/Controllers/Validador/Hogar/ValidarMortalidad.php
+++ app/Http/Controllers/Validador/Hogar/ValidarMortalidad.php
@@ -14,23 +14,20 @@ class ValidarMortalidad extends ValidacionHogar implements ValidacionEncuesta
 
     public function validar()
     {
-        $this->puntuacion('fallecido_familiar');
-        $this->puntuacion('sexo_fallecido');
-        $this->puntuacion('edad_fallecido');
-        $this->puntuacion('causa_muerte');
-        $this->puntuacion('fecha_muerte');
+        $this->fallecidos();
     }
 
     protected function fallecidos()
     {
         {
             $fallecido = $this->puntuacion('fallecido_familiar');
-            //si la persona si tiene un familiar fallecido recientemente
-            $this->validacionSimple('sexo_fallecido', ($fallecido->id == 381));
-            $this->validacionSimple('edad_fallecido', ($fallecido->id == 381));
-            $this->validacionSimple('cusa_muerte', ($fallecido->id == 381));
-            $this->validacionSimple('fecha_muerte', ($fallecido->id == 381));
-          
+            if ($fallecido->id == 381)
+            {
+                $this->puntuacion('sexo_fallecido');
+                $this->puntuacion('edad_fallecido');
+                $this->puntuacion('cusa_muerte');
+                $this->puntuacion('fecha_muerte');
+            }
         }
     }
 }
