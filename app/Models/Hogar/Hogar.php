b8724b146500775b91193d304dcd1abeb61f8100
Sergio Blanco <sandresbg@hotmail.com>
2023-01-25 19:54:27 -0500

    id usuarios en hogar e integrantes

--- app/Models/Hogar/Hogar.php
+++ app/Models/Hogar/Hogar.php
@@ -23,6 +23,7 @@ class Hogar extends Model
     protected $keyType = 'uuid';
     protected $fillable = [
         'id',
+        'id_usuarios',
         'barrio_vereda_id',
         'zona',
         'cod_dpto',
