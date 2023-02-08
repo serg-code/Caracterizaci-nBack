e7517271256ad515f645a050fcebe95f10bf1153
desteban <esteban28c@hotmail.com>
2023-02-08 12:39:06 -0500

    guardar datos en la base de datos

--- app/Imports/CargadorImport.php
+++ app/Imports/CargadorImport.php
@@ -11,12 +11,16 @@ use Maatwebsite\Excel\Concerns\ToCollection;
 class CargadorImport implements ToCollection
 {
     protected array $columnas;
+    protected string $nombreTabla;
+    private int $cantidadColumna;
 
     public function __construct(
         protected Cargadores $cargador
     )
     {
         $this->columnas = CargadoresColumns::where('id_cargador', '=', $this->cargador->id)->get()->toArray();
+        $this->cantidadColumna = sizeof($this->columnas);
+        $this->nombreTabla = str_replace(' ', '_', $this->cargador->nombre);
     }
 
     /**
@@ -30,11 +34,20 @@ class CargadorImport implements ToCollection
             // for ($i = 0; $i < $this->cantidadColumnas; $i++) {
             //     $datoGuardar[$this->nombreColumnas[$i]] = $columna[$i];
             // }
+            for ($i = 0; $i < $this->cantidadColumna; $i++) {
+                try {
+
+                    $datoGuardar[$this->columnas[$i]['nombre']] = $columna[$i];
+                } catch (\Throwable $th) {
+                    //throw $th;
+                    // dd($this->columnas[$i]['nombre']);
+                    dd($columna);
+                }
+            }
 
             return $datoGuardar;
         }, $collection->toArray());
-
-        // DB::table($this->nombreTabla)->insertOrIgnore($datos);
+        DB::table($this->nombreTabla)->insertOrIgnore($datos);
     }
 
 }
